63a320f90be6dae12fae636ed21694af8218ef70
Andre <3oliva41@solent.ac.uk>
2020-01-16 00:28:21 +0000

    Needs fixing when uploading data to mongodb

--- Python/insertData.py
+++ Python/insertData.py
@@ -11,42 +11,58 @@ client= MongoClient(host)
 db = client["gw_data"]
 col_name =  input("Please enter the new collection name: ")
 col = db[col_name]
+run = True
 
 def main():
-        
-	#print the files in the current directory
-	cwd = os.getcwd()
-	for subdir, dirs, files in os.walk(cwd, topdown=True):
-		del dirs[:] #remove sub directories.
-		for file in files:
-			print (file)
+    while(run):
+        #print the files in the current directory and store them in array
+        list_files=[]
+        cwd = os.getcwd()
+    
+        for subdir, dirs, files in os.walk(cwd, topdown=True):
+            del dirs[:] #remove sub directories.
+            for file in files:
+                list_files.append(file)
+                print(file)
 
-	file_to_load = input("File to load: ")
+        file_to_load = input("File to load: ")
+    
+        file_exists = doesFileExist(file_to_load, list_files)
 
-	loadAndInsertData(file_to_load)
+        if(file_exists):
+            loadAndInsertData(file_to_load)    
+        else:
+            print('\n File does not exist, please try again')
+
+def doesFileExist(file_name, file_list):
+     exists = False
+     if(file_name in file_list):
+        exists = True
+     return exists
 
 
 def loadAndInsertData(file):
-	#str() to make sure the file path is in string
-	csvfile = open(str(file))
-	reader = csv.DictReader(csvfile)
-	header = reader.fieldnames
-	count = 0
+    #str() to make sure the file path is in string
+    csvfile = open(str(file))
+    reader = csv.DictReader(csvfile)
+    header = reader.fieldnames
+    count = 0
+    try: 
+        for each in reader:
+            row={}
+            for field in header:
+                row[field]=each[field]
+                col.insert_one(row)
+                count = count + 1
 
-	for each in reader:
-		row={}
-		for field in header:
-			row[field]=each[field]
-		col.insert_one(row)
-		count = count + 1
-
-	if(count!=0):
-		print("COMPLETED! " + str(count) + " rows inserted")
-	else:
-		print(str(count) + " rows have been inserted")
-
+        if(count!=0):
+            print("COMPLETED! " + str(count) + " rows inserted")
+        else:
+            print(str(count) + " rows have been inserted")
+    except:
+        print('\n Something went wrong')
 
 
 if __name__=="__main__":
-	main()
+    main()
 
--- Python/makeGraph.py
+++ Python/makeGraph.py
@@ -11,35 +11,34 @@ col = db["methane_emissions"]
 
 
 def main():
-	years = []
-	country = input("Country? ")
-	values = []
+    years = []
+    country = input("Country? ")
+    values = []
 
-	q = {"GEO":country}
+    q = {"GEO":country}
 
-	doc = col.find(q)
+    doc = col.find(q)
 
-	for x in doc:
-		years.append(x["TIME"])
-		values.append(float(x["Value"].replace(",","")))
+    for x in doc:
+        years.append(x["TIME"])
+        values.append(float(x["Value"].replace(",","")))
 
 
 
-	x_pos=[i for i, _ in enumerate(years)]
+    x_pos=[i for i, _ in enumerate(years)]
 
-	plt.bar(x_pos, values, color="green")
-	plt.xlabel("Year")
-	plt.ylabel("Mehtane emitted (Thousands of tons)")
+    plt.bar(x_pos, values, color="green")
+    plt.xlabel("Year")
+    plt.ylabel("Mehtane emitted (Thousands of tons)")
 
-	#plt.axis([xmin, xmax, ymin, ymax])
-	#By setting the x values to None matplotlib will work it out itself
-	plt.axis([None, None, round(min(values)) - 100, round(max(values)) + 50])
+    #plt.axis([xmin, xmax, ymin, ymax])
+    #By setting the x values to None matplotlib will work it out itself
+    plt.axis([None, None, round(min(values)) - 100, round(max(values)) + 50])
 
-	plt.title(country + "'s Methane Emissions")
+    plt.title(country + "'s Methane Emissions")
 
-	plt.xticks(x_pos, years)
-	plt.show()
-
+    plt.xticks(x_pos, years)
+    plt.show()
 
 
 
